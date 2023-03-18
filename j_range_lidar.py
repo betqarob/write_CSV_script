b8601abcc0592f2fe2a8a9c3a2cb0b2cbc9567cf
Robert Cueto <55465518+betqarob@users.noreply.github.com>
2023-03-17 21:21:41 -0700

    Add files via upload
    range lidar connects to the Lidars to collect data and script_csv creates a csv file to store the data.

---
+++ j_range_lidar.py
@@ -0,0 +1,59 @@
+import serial, time
+from script_csv import * 
+
+SIZE = 2
+BAUD_RATE = 115200
+DEV = "/dev/ttyUSB"
+
+my_serial = []
+
+for x in range(SIZE):
+    my_serial.append(serial.Serial(DEV + str(x), BAUD_RATE))
+
+class RangeRead:
+    '''
+    set to mm for measurement unit
+    command 5A 05 05 06 6A - set to mm
+    command 5A 05 05 01 65 - set to cm
+    command 5A 04 11 6F - save settings
+    ser.write ('5A 05 05 01 65')
+    '''
+    def __init__(self):
+        self.init()
+
+    #configures all the lidars to be set to mm measurement
+    def init(self):
+        for x in range(len(my_serial)):
+            my_serial[x].write('5A 05 05 01 65')
+            my_serial[x].write('5A 04 11 6F')
+            my_serial[x].reset_input_buffer()
+    
+    # modified the function to read from all devices and store it into a csv script.
+    def getTFminiData(self):
+        while True:
+            for x in range(SIZE):
+                count = my_serial[x].in_waiting
+                if count > 9:
+                    recv = my_serial[x].read(9)
+                    my_serial[x].reset_input_buffer()
+                    if (recv[0] == 'Y' and recv[1] == 'Y'):
+                        low = int(recv[2].encode('hex'), 16)
+                        high = int(recv[3].encode('hex'), 16)
+                        distance = low + (high << 8)
+                        script.data_append(distance, x)
+                        print(distance, 'mm')
+                        time.sleep(.5)
+            script.append_csv('testing1.csv')
+
+# modified the main to close all devices and create the csv file.
+if __name__ == '__main__':
+    try:
+        script = script_csv('testing1.csv')
+        if my_serial[0].is_open == False:
+            my_serial[0].open()
+        reader = RangeRead()
+        reader.getTFminiData()
+    except KeyboardInterrupt: #Ctrl+C
+        if my_serial[0] != None:
+            my_serial[0].close()
+        script.remove_blanks('testing1.csv', 'testing_clean1.csv')
---
+++ script_csv.py
@@ -0,0 +1,61 @@
+import csv, os
+
+test = ['Lidar 1', 'Lidar 2']
+data_list = ["Null", "Null"]
+
+
+class script_csv:
+	'''
+    takes in information from range_lidar.py and stores the data from each lidar into the 
+    data_list to have it in the right order when writing in the .csv file.
+    Keeping in mind hardware latency, make the data_list be filled with dummy items "Null"
+    '''
+	# checks if the file is already created, if it is it will append data
+	# if not, it creates a new one.
+	def is_csv(self, file):
+		if (not(os.path.isfile(file))):
+			print("File doesn't not exist! Creating one now...")
+			self.create_csv(file)
+	# creating the csv file
+	def create_csv(self, file):
+		with open(file, 'w') as f:
+			filewriter = csv.writer(f, delimiter=',', lineterminator='\n',
+						quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			filewriter.writerow(test)
+
+	# stores the data from ever loop in range_lidar into global variable to be called after every loop
+	def data_append(self, data, x):
+		global data_list
+		# if the list is empty, it fills the list with dummy items "Null"
+		if (len(data_list) == 0):
+			data_list = ["Null"]*2
+
+		# checks if index matches for loop index, if so, swithc dummy item for given data.
+		for i in range(len(test)):
+			if (x == i) and (data != None):
+				data_list[i] = data
+			else:
+				pass
+	
+
+	# appends the data_list into the row in order to fill in the column spaces.
+    # clears the list to store new data for the following row.
+	def append_csv(self, file):
+		global data_list
+		with open(file, 'a') as f:
+			filewriter = csv.writer(f, delimiter=',', lineterminator='\n',
+                                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			filewriter.writerows([data_list])
+			data_list = []
+
+	# post processing for the csv file; remove empty rows to make it readable for users.
+	def remove_blanks(self, file, file1):
+		with open(file, 'r') as in_file:
+			with open(file1, 'w') as out_file:
+				writer = csv.writer(out_file)
+				for row in csv.reader(in_file):
+					if any(row):
+						writer.writerow(row)
+	
+	
+
